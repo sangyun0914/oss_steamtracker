2ae0822b644a5fb3eec1af431d3b3f024c42e11e
tool7831 <tool7831@gmail.com>
2022-11-15 19:55:31 +0900

    +add lowest price order highest price order lowest discount rate order highest discount rate order search game title

---
+++ sort_search/sort_search.js
@@ -0,0 +1,64 @@
+
+const jsonData = require('../web-scraping/scraped.json');
+let game_data = JSON.parse(JSON.stringify(jsonData));
+game_data = game_data.filter(game => game['discount rate'] !== null);
+
+//lowest price order
+function lowest_price(game_list) {
+    game_list.sort((a, b) => a.discounted - b.discounted);
+}
+//highest price order
+function highest_price(game_list) {
+    game_list.sort((a, b) => b.discounted - a.discounted);
+}
+//lowest discount rate order
+function lowest_rate(game_list) {
+    game_list.sort((a, b) => parseInt(b["discount rate"]) - parseInt(a["discount rate"]));
+}
+//highest discount rate order
+function highest_rate(game_list) {
+    game_list.sort((a, b) => parseInt(a["discount rate"]) - parseInt(b["discount rate"]));
+}
+//search title
+function search_title(game_list, substirng) {
+    return game_list.filter(game => game.title.indexOf(substirng) !== -1)
+}
+
+// load game list
+function load_list(user_select) {
+
+    let game_list = game_data.filter(game => { return 1 });
+
+    if (user_select.order === Order_type['lowest price'])
+        lowest_price(game_list);
+    else if (user_select.order === Order_type['higest price'])
+        highest_price(game_list);
+    else if (user_select.order === Order_type['lowest rate'])
+        lowest_rate(game_list);
+    else if (user_select.order === Order_type['higest rate'])
+        highest_rate(game_list);
+
+
+    if (user_select.title)
+        game_list = search_title(game_list, user_select.title);
+
+    return game_list;
+}
+
+const Order_type = {
+    default: 0,
+    "lowest price": 1,
+    "higest price": 2,
+    "lowest rate": 3,
+    "higest rate": 4,
+}
+
+
+// exmaple
+let user_select = {
+    order: Order_type['lowest rate'],
+    title: null,
+}
+
+let game_list = load_list(user_select);
+console.log(game_list);
