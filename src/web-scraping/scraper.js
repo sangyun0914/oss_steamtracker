a4d49e25cd3a319fe05b53326a876c77d8c423d2
신상윤 <sangyun0914@g.skku.edu>
2022-11-29 20:59:55 +0900

    Modify price type to int

--- src/web-scraping/scraper.js
+++ src/web-scraping/scraper.js
@@ -4,45 +4,57 @@ const cheerio = require("cheerio");
 let url =
   "https://store.steampowered.com/search/results/?query&start=0&count=50&dynamic_data=&sort_by=_ASC&os=win&supportedlang=english&snr=1_7_7_7000_7&filter=topsellers&infinite=1";
 
-function get_data(url) {
+function parseSteam(url) {
   https.get(url, (res) => {
     let data = "";
     res.on("data", (d) => {
       data += d;
     });
+    // Parse given data
     res.on("end", () => {
       let result = JSON.parse(data);
       result = result["results_html"];
 
-      let gameslist = [];
+      let gameslist = []; // Total games array of game objects
       const $ = cheerio.load(result);
       const list = $("a");
-      list.each((i, tag) => {
-        let title = $(tag).find(".title").text();
 
-        let discount_rate = $(tag).find(".search_discount").text().trim();
-        if (discount_rate === "") discount_rate = undefined;
-        let price;
-        let discounted;
+      // Loop through games
+      list.each((i, tag) => {
+        let title, discount_rate, price, discounted, imgSmall, imgBig, link;
+
+        title = $(tag).find(".title").text();
+
+        discount_rate = $(tag).find(".search_discount").text().trim();
+
+        if (discount_rate === "") discount_rate = null;
+
         try {
-          price = $(tag)
-            .find(".search_price")
-            .text()
-            .trim()
-            .split("₩")[1]
-            .trim();
+          price = parseInt(
+            $(tag)
+              .find(".search_price")
+              .text()
+              .trim()
+              .split("₩")[1]
+              .trim()
+              .replaceAll(",", "")
+          );
         } catch {
-          price = "0";
+          price = 0;
         }
+
         try {
-          discounted = $(tag)
-            .find(".search_price")
-            .text()
-            .trim()
-            .split("₩")[2]
-            .trim();
+          discounted = parseInt(
+            $(tag)
+              .find(".search_price")
+              .text()
+              .trim()
+              .split("₩")[2]
+              .trim()
+              .replaceAll(",", "")
+          );
         } catch {
-          discounted = "0";
+          discounted = 0;
         }
 
         mygame = {
@@ -56,19 +68,8 @@ function get_data(url) {
       });
 
       console.log(gameslist);
-
-      //const games = root.querySelectorAll("a");
-      // console.log(games);
-
-      // games.each((game) => {
-      //   console.log(game);
-      // });
     });
   });
 }
 
-function getTopSeller() {
-  let data = get_data(url);
-}
-
-getTopSeller();
+parseSteam(url);
