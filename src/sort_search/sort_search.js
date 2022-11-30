d2a39e8f269770e8b950331cf140e225a7a82e52
tool7831 <tool7831@gmail.com>
2022-11-30 16:32:52 +0900

    add platform selection

--- src/sort_search/sort_search.js
+++ src/sort_search/sort_search.js
@@ -2,15 +2,15 @@ const jsonData = require('../web-scraping/scraped.json');
 let game_data = JSON.parse(JSON.stringify(jsonData));
 
 //lowest price order
-export function lowest_price(game_list) {
+/*export*/ function lowest_price(game_list) {
     game_list.sort((a, b) => a.discounted - b.discounted)
 }
 //highest price order
-export function highest_price(game_list) {
+/*export*/ function highest_price(game_list) {
     game_list.sort((a, b) => b.discounted - a.discounted)
 }
 //lowest discount rate order
-export function lowest_rate(game_list) {
+/*export*/ function lowest_rate(game_list) {
     game_list.sort((a, b) => {
         let _a = a["discount rate"] !== null ? parseInt(a["discount rate"]) : 0;
         let _b = b["discount rate"] !== null ? parseInt(b["discount rate"]) : 0;
@@ -18,7 +18,7 @@ export function lowest_rate(game_list) {
     });
 }
 //highest discount rate order
-export function highest_rate(game_list) {
+/*export*/ function highest_rate(game_list) {
     game_list.sort((a, b) => {
         let _a = a["discount rate"] !== null ? parseInt(a["discount rate"]) : 0;
         let _b = b["discount rate"] !== null ? parseInt(b["discount rate"]) : 0;
@@ -26,7 +26,7 @@ export function highest_rate(game_list) {
     });
 }
 //lowest rating order
-export function lowest_rating(game_list) {
+/*export*/ function lowest_rating(game_list) {
     game_list.sort((a, b) => {
         let _a = a.rating !== null ? a.rating.toString().slice(0, 2) : 0;
         let _b = b.rating !== null ? b.rating.toString().slice(0, 2) : 0;
@@ -34,7 +34,7 @@ export function lowest_rating(game_list) {
     });
 }
 //highest rating order
-export function highest_rating(game_list) {
+/*export*/ function highest_rating(game_list) {
     game_list.sort((a, b) => {
         let _a = a.rating !== null ? a.rating.toString().slice(0, 2) : 0;
         let _b = b.rating !== null ? b.rating.toString().slice(0, 2) : 0;
@@ -42,28 +42,34 @@ export function highest_rating(game_list) {
     });
 }
 //search title
-export function search_title(game_list, substirng) {
+/*export*/ function search_title(game_list, substirng) {
     return game_list.filter((game) => game.title.indexOf(substirng) !== -1)
+}
+//select platform
+/*export*/ function select_platform(game_list, platform) {
+    return game_list.filter((game) => game.platform.some((element) => element === platform))
 }
 
 // load game list
-export function load_list(user_select) {
+/*export*/ function load_list(user_select) {
     let game_list = game_data.filter((game) => {
         return 1
     })
-    if (user_select.title) game_list = search_title(game_list, user_select.title)
 
-    if (user_select === 1) lowest_price(game_list)
-    else if (user_select === 2) highest_price(game_list)
-    else if (user_select === 3) lowest_rate(game_list)
-    else if (user_select === 4) highest_rate(game_list)
+    if (user_select.title) game_list = search_title(game_list, user_select.title)
+    if (user_select.platform) game_list = select_platform(game_list, user_select.platform)
+
+    if (user_select.order === 1) lowest_price(game_list)
+    else if (user_select.order === 2) highest_price(game_list)
+    else if (user_select.order === 3) lowest_rate(game_list)
+    else if (user_select.order === 4) highest_rate(game_list)
     else if (user_select.order === 5) lowest_rating(game_list)
     else if (user_select.order === 6) highest_rating(game_list)
 
     return game_list
 }
 
-export const Order_type = {
+/*export*/ const Order_type = {
     default: 0,
     'lowest price': 1,
     'higest price': 2,
@@ -74,10 +80,11 @@ export const Order_type = {
 }
 
 // exmaple
-export let user_select = {
+/*export*/ let user_select = {
     order: Order_type['lowest rate'],
     title: null,
+    platform: "win",
 }
 
-export let game_list = load_list(user_select)
+/*export*/ let game_list = load_list(user_select)
 console.log(game_list)
